333d7ddc04117f04916b40ddb6fa00fd957389d4
rubikwebsite3-star <rubikwebsite3@gmail.com>
2026-04-04 23:57:41 +0530

    Update settings.py

--- rubikproject/settings.py
+++ rubikproject/settings.py
@@ -51,26 +51,22 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'rubikproject.urls'
+
+
 TEMPLATES = [
     {
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
 
 WSGI_APPLICATION = 'rubikproject.wsgi.application'
 
